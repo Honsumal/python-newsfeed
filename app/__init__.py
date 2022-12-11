3708f7d0c74398589ce25c3cb8afd4ea556c4aed
honsumal <14leealastair@gmail.com>
2022-12-11 17:58:09 -0500

    added dashb

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from app.routes import home
+from app.routes import home, dashboard
 
 def create_app(test_config=None):
   # set up app config
@@ -11,7 +11,8 @@ def create_app(test_config=None):
   @app.route('/hello')
   def hello():
     return 'hello world'
-    
+
   app.register_blueprint(home)
+  app.register_blueprint(dashboard)
 
   return app
